b0f83c3c068e1d0b59b437b9f5e7bdcbc38d3188
shivanigehlot3 <144298742+shivanigehlot3@users.noreply.github.com>
2025-02-18 00:59:02 +0530

    Create diff.java
    difference of 2 numbers

---
+++ diff.java
@@ -0,0 +1,7 @@
+ublic class diff {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20;
+        int sum = num1 - num2;
+        System.out.println("Sum: " + sum);
+    }
+}
